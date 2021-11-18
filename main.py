ed9130f266d616f104adadab8084b95a1b6a8471
mbdavid2 <mbdavid297@gmail.com>
2021-11-18 21:11:16 +0000

    Added Network class with multiple layers

--- main.py
+++ main.py
@@ -13,25 +13,30 @@ biases = [2, 3, 0.5]
 np.random.seed(0)
 biases = np.random.rand(1, 4)
 
-inputs = [[1, 2, 3, 2.5],
+inputs = [[1, 2, 3],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5, 2.7, 3.3, -0.8]]
 
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+# output = np.dot(inputs, np.array(weights).T) + biases
+# print(output)
 
 
-sigmoid = mlp.ActivationFunction()
-nNeurons = 4
-neurons = []
-for i in range(0, nNeurons):
-    neurons.append(mlp.Neuron(sigmoid))
+# sigmoid = mlp.ActivationFunction()
+# nNeurons = 4
+# neurons = []
+# for i in range(0, nNeurons):
+#     neurons.append(mlp.Neuron(sigmoid))
 
-print(neurons)
+# print(neurons)
 
-for neuron in neurons:
-    print(neuron.activate(3))
+# for neuron in neurons:
+#     print(neuron.activate(3))
 
-layer = mlp.Layer(4, neurons)
-layer.forwardPropagation(inputs)
-print(layer.getOutput())
+hiddenLayer = mlp.Layer(3, 4)
+# outputLayer = mlp.Layer(hiddenLayer.getLayerSize(), 3)
+
+network = mlp.Network([hiddenLayer])
+network.feedForward(inputs[0])
+
+# layer.forwardPropagation(inputs)
+# print(layer.getOutput())
--- mainClasses.py
+++ mainClasses.py
@@ -1,11 +1,20 @@
 import numpy as np
 
 class ActivationFunction:
-    def activate(self, input):
-        if input > 0.5:
+    def activate(self, inputValue):
+        return 0*inputValue
+
+
+class Sigmoid(ActivationFunction):
+    def activate(self, inputValue):
+        if inputValue > 0.5:
             return 1
         else:
             return 0
+
+class Same(ActivationFunction):
+    def activate(self, inputValue):
+        return inputValue
 
 
 class Neuron:
@@ -17,24 +26,62 @@ class Neuron:
     
 
 class Layer:
-    def __init__(self, inputSize, neurons):
+    def __init__(self, inputSize, size, activation = Same()):
         """dasdasda
 
         Keyword arguments:
         inputSize -- expected input size
-        neurons -- list of neurons (type neuron)
+        size -- number of neurons
+        activationFunc -- activation function
         """
         self.inputSize = inputSize
-        self.neuronNumber = len(neurons)
+        self.size = size
+        self.neurons = []
+        self.activation = activation
+
+        sigmoid = ActivationFunction()
+        for i in range(0, self.size):
+            self.neurons.append(Neuron(sigmoid))
 
         # Random initial weights and biases
         np.random.seed(0)
-        self.weights = np.random.rand(self.inputSize, self.neuronNumber)
+        self.weights = np.random.rand(self.inputSize, self.size)
         np.random.seed(0)
-        self.biases = np.random.rand(1, self.neuronNumber)
+        self.biases = np.random.rand(1, self.size)
+
+
+    def getInputSize(self):
+        return self.inputSize
+
+    def getLayerSize(self):
+        return self.size
+
 
     def forwardPropagation(self, inputData):
-        self.output = np.dot(inputData, np.array(self.weights).T) + self.biases
+        print("Weights shape: ", self.weights.shape)
+        print("Input size: ", len(inputData))
+        self.output = np.dot(self.weights.T, inputData) + self.biases
 
     def getOutput(self):
-        return self.output
+        return [self.activation.activate(sum(x)) for x in self.output]
+
+
+class Network:
+    def __init__(self, layers):
+        self.layers = layers
+
+    def feedForward(self, inputData):
+        # Check that the input size is the same as layers[0]
+        if len(inputData) != self.layers[0].getInputSize():
+            errorStr = ('Input size: ' + str(len(inputData)) + 
+                       ' != first layer size: ' + str(self.layers[0].getInputSize()))
+            raise ValueError(errorStr)
+
+        nextInput = inputData
+        i = 0
+        for layer in self.layers:
+            print('Layer', i)
+            layer.forwardPropagation(nextInput)
+            nextInput = layer.getOutput()
+            print("Output of the layer: ", nextInput)
+            i = i + 1
